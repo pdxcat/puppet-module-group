0c11981e674ddd55bfbb084bdafc1c524a0e2331
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-02-25 15:15:30 -0800

    Revert boolean logic fix

--- lib/puppet/provider/group/gfilegroupadd.rb
+++ lib/puppet/provider/group/gfilegroupadd.rb
@@ -72,7 +72,7 @@ Puppet::Type.type(:group).provide(:gfilegroupadd) do
       groupfile_path = '/etc/group'
       groupfile_tmp = File.open(groupfile_path_tmp, 'w')
       groupfile = File.foreach(groupfile_path) do |line|
-        if groupline == line.match(/^#{@resource[:name]}:[x*]?:[0-9]+:/)
+        if groupline = line.match(/^#{@resource[:name]}:[x*]?:[0-9]+:/)
           Puppet.debug "Writing members " << value.join(',') << " to " << groupfile_path_tmp
           groupfile_tmp.puts groupline[0] + value.join(',')
         else
